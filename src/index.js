9f27e6497875f8e5f494f07e08a87034b23cf72f
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-17 20:45:05 -0500

    fix dependencies combatiblity problem

--- src/index.js
+++ src/index.js
@@ -2,18 +2,9 @@
 import React from 'react';
 //import render method
 import { render } from 'react-dom';
-//import the app component
-import { App } from './components/App';
-//import context provider
-import { ContextProvider } from '../src/utils/Context';
 
 //crete root const
 const root = document.getElementById('root');
 
 //render the app
-render(
-	<ContextProvider>
-			<App />
-	</ContextProvider>,
-	root,
-);
+render(<h1>Im alive</h1>, root);
