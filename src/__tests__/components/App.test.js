90927ed5fcf7ed887e0e62eb2cc37e7f2b336c58
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-18 20:30:30 -0500

    Fix test problems

---
+++ src/__tests__/components/App.test.js
@@ -0,0 +1,16 @@
+// import react
+import React from 'react';
+// import shallow
+import { shallow } from 'enzyme';
+// import app component
+import App from '../../components/App';
+// import toJson
+//import toJson from 'enzyme-to-json';
+
+describe('<App />', () => {
+  const wrapper = shallow(<App />);
+
+  it('is render the app?', () => {
+    expect(wrapper.exists()).toBe(true)
+  });
+});
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,10 +2,12 @@
 import React from 'react';
 
 //create and export app component
-const App = () => (
-  <>
-    <h1>I&apos;m the app component xd</h1>
-  </>
-);
+const App = () => {
+  return (
+    <>
+      <h1>I&apos;m the app component xd</h1>
+    </>
+  );
+};
 
 export default App;
