210ee1157076e5025fbc900c2066f50b86b3479f
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-19 17:37:44 -0500

    Add styles to hero component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,13 @@
 // import react
 import React from 'react';
+// use styles
+import './styles.styl'
 
 // create and export Hero component
 const Hero = () => (
-  <>
+  <div className="home__hero-container">
     <h1>I&apos;m the Hero</h1>
-
-  </>
+  </div>
 );
 
 export default Hero;
