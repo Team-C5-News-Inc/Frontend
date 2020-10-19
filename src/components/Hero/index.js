d28109a2b118c13010f5faefcfc73bdeb1a0e95b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-19 18:50:52 -0500

    Update with succes hero styles

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,8 +8,8 @@ const Hero = ({ background }) => (
   <div className="home__hero-container">
     <figure className="home__hero-figure">
       <img className="home__hero-image" src={background}/>
+      <h1 className="home__hero-caption">I&apos;m the Hero</h1>
     </figure>
-    <h1 className="home__hero-caption">I&apos;m the Hero</h1>
   </div>
 );
 
