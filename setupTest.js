94bc643eeedbc906832945e99affe214b0b0f30a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-19 16:12:20 -0500

    update readme

--- setupTest.js
+++ setupTest.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { configure } from 'enzyme';
-// eslint-disable-next-line import/no-extraneous-dependencies
+
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
