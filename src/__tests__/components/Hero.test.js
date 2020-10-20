0798527606626f7567f8d6df40513e98db636de1
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-19 20:29:50 -0500

    Update the test for hero component

--- src/__tests__/components/Hero.test.js
+++ src/__tests__/components/Hero.test.js
@@ -16,8 +16,12 @@ describe('<Hero />', () => {
     expect(wrapper.find('.home__hero-container').exists()).toBe(true)
   });
 
-  it('are container have one child?', () => {
-    expect(wrapper.find('.home__hero-container').children.length).toBe(1)
+  it('are container have two children?', () => {
+    expect(wrapper.find('.home__hero-container').children().length).toBe(2)
   });
+
+  it('should have home hero class container?', () => {
+    expect(wrapper.hasClass('home__hero-container')).toBe(true)
+  })
 
 });
