5add880cc0eb90be5b498af88d4320fcda1c9dc4
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-19 20:19:16 -0500

    Improve styles of hero component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,7 @@ import Hero from '../../components/Hero'
 const Home = () =>
   (<>
   I&apos;m the Home page
-    <Hero caption="The puppy's are so beautiful" background="https://images.unsplash.com/photo-1601907532353-3c9176919f38?ixlib=rb-1.2.1&auto=format&fit=crop&w=982&q=80" />
+    <Hero caption="Top 10 amazing things 2020" background="https://images.unsplash.com/photo-1601907532353-3c9176919f38?ixlib=rb-1.2.1&auto=format&fit=crop&w=982&q=80" />
   </>)
 
 export default Home;
