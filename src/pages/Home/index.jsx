bc9f34358cef4aefe0392afe2f90a5ffbc0bf96c
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-20 13:56:48 -0500

    Create mucks for files

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ import Hero from '../../components/Hero/index.jsx'
 // create and export Home page
 const Home = () =>
   (<>
-  I&apos;m the Home page
+  I&apos;m the Home page :/
     <Hero caption="Top 10 amazing things 2020" background="https://images.unsplash.com/photo-1601907532353-3c9176919f38?ixlib=rb-1.2.1&auto=format&fit=crop&w=982&q=80" />
   </>)
 
