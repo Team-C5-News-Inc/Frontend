e8e24a7c1567d0611074cf28ad7c040a8eb817fd
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-20 18:10:40 -0500

    Update hero tests with the new implementations

--- src/__tests__/components/Hero.test.js
+++ src/__tests__/components/Hero.test.js
@@ -2,26 +2,51 @@
 import React from 'react';
 // import shallow
 import { shallow } from 'enzyme';
+// import toJson
+import toJson from 'enzyme-to-json';
 // import app component
 import Hero from '../../components/Hero/index.jsx';
+
+describe('<Hero type=small/>', () => {
+  const wrapper = shallow(<Hero type="small" />);
+
+  it('is render the Hero?', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('are there a container?', () => {
+    expect(wrapper.find('.home__hero-container').exists()).toBe(true);
+  });
+
+  it('are container have two children?', () => {
+    expect(wrapper.find('.home__hero-container').children().length).toBe(2);
+  });
+
+  it('should have home hero class container?', () => {
+    expect(wrapper.hasClass('home__hero-container')).toBe(true);
+  });
+
+  it('are there correct the type?', () => {
+    const type = '';
+
+    expect(
+      wrapper.containsMatchingElement(
+        <div
+          className={`home__hero-container ${type === 'small' && 'small'}`}
+        />,
+      ),
+    );
+  });
+
+  it('matches the snap shot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
 
 describe('<Hero />', () => {
   const wrapper = shallow(<Hero />);
 
-  it('is render the Hero?', () => {
-    expect(wrapper.exists()).toBe(true)
-  });
-
-  it('are there a container?', () => {
-    expect(wrapper.find('.home__hero-container').exists()).toBe(true)
-  });
-
-  it('are container have two children?', () => {
-    expect(wrapper.find('.home__hero-container').children().length).toBe(2)
-  });
-
-  it('should have home hero class container?', () => {
-    expect(wrapper.hasClass('home__hero-container')).toBe(true)
+  it('are default type works?', () => {
+    expect(wrapper.exists()).toBeTruthy()
   })
-
-});
+})
