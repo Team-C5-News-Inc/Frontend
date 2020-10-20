259be88da8bd36749aca9efaa24b675cbdd43fdf
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-20 16:19:09 -0500

    Use react router Dom

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,16 @@
 // import react
 import React from 'react';
+// import hash router
+import { HashRouter as Router } from 'react-router-dom';
 // import Home page
 import Home from '../../pages/Home/index.jsx';
 
 //create and export app component
 const App = () => {
   return (
-    <>
+    <Router basename="/">
       <Home />
-    </>
+    </Router>
   );
 };
 
