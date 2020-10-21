e50e9ffa78b31761f4b23206e6b6f6045d57ac28
Xavier Garcia <xaviergn60@gmail.com>
2020-10-21 06:51:27 -0600

    need some more styles

--- src/components/Masonry/index.js
+++ src/components/Masonry/index.js
@@ -13,13 +13,23 @@ const Grid = () => {
       <ResponsiveMasonry
         columnsCountBreakPoints={{ 480: 1, 768: 2, 1024: 3 }}
       >
-        <Masonry gutter="10px">
+        <Masonry gutter="20px">
           <Card title="Milos Foreman saca un nuevo disco" image="https://i.imgur.com/umQqEfs.png"/>
           <Card title="Jorge Garcia saca a pasear a la cheve" image="https://i.imgur.com/ZiGz0xk.jpg"/>
           <Card title="Edicion 20 de checa mi setup" image="https://i.imgur.com/RbQI86x.jpg"/>
           <Card title="Milos Foreman saca un nuevo disco" image="https://i.imgur.com/QORYDiJ.jpg"/>
           <Card title="Jorge Garcia saca a pasear a la cheve" image="https://i.imgur.com/zGDQOk1.jpg"/>
           <Card title="Edicion 20 de checa mi setup" image="https://i.imgur.com/7WYSsLw.jpg"/>
+          <Card title="Jorge Garcia saca a pasear a la cheve" image="https://i.imgur.com/9A6mOm0.jpg"/>
+          <Card title="Edicion 20 de checa mi setup" image="https://i.imgur.com/5tzodZa.jpg"/>
+          <Card title="Milos Foreman saca un nuevo disco" image="https://i.imgur.com/umQqEfs.png"/>
+          <Card title="Jorge Garcia saca a pasear a la cheve" image="https://i.imgur.com/ZiGz0xk.jpg"/>
+          <Card title="Edicion 20 de checa mi setup" image="https://i.imgur.com/RbQI86x.jpg"/>
+          <Card title="Milos Foreman saca un nuevo disco" image="https://i.imgur.com/QORYDiJ.jpg"/>
+          <Card title="Jorge Garcia saca a pasear a la cheve" image="https://i.imgur.com/zGDQOk1.jpg"/>
+          <Card title="Edicion 20 de checa mi setup" image="https://i.imgur.com/7WYSsLw.jpg"/>
+          <Card title="Jorge Garcia saca a pasear a la cheve" image="https://i.imgur.com/9A6mOm0.jpg"/>
+          <Card title="Edicion 20 de checa mi setup" image="https://i.imgur.com/5tzodZa.jpg"/>
           <Card title="Jorge Garcia saca a pasear a la cheve" image="https://i.imgur.com/9A6mOm0.jpg"/>
           <Card title="Edicion 20 de checa mi setup" image="https://i.imgur.com/5tzodZa.jpg"/>
           <Card title="Milos Foreman saca un nuevo disco" image="https://i.imgur.com/umQqEfs.png"/>
