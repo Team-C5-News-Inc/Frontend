59204c79b58ce3938e106434afb7432e1cb71502
Leandro Velasco <pino2110@gmail.com>
2020-10-21 12:59:28 -0500

    fix - icons and styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,26 +3,33 @@ import React from 'react';
 
 import '../Header/Header.styl'
 import Logo from '../../assets/Logo'
-//import arrow from '../../assets/arrow.svg'
+//import arrow from '../../assets/arrow2.png'
 
 //create and export app component
 const Header = () =>  (
   <header>
 		<nav>
-			<a href="#" className="icon_logo">
+			<a href="#">
 				<Logo />
 			</a>
 			<ul className="nav-right-section">
-				<li className="search">
-					<div> 
-					<a href="#">
-  						<img src="" alt="" />
-  					Search
-					</a>
+				<li>
+					<div className="searchContainer">
+						<div id="searchform" className="searchform"> 
+							<form action="#" name="top_search">  
+								<input className="searchbox" name="q" type="search" /> 
+							</form> 
+						</div> 
+						<a href="#" className="search">
+							<i className="fa fa-search fa-2x"></i>
+						</a>
+					
 					</div> 
 				</li>
 				<li className="menu_icon">
-					<a href="#">Menu</a>
+					<a href="#" id="arrow">
+						<i className="fa fa-angle-down fa-4x"></i>
+					</a>
 				</li>
 			</ul>
 		</nav>
