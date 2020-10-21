4c0bfde1c65f40ef4c0efbad614dea1348294382
Xavier Garcia <xaviergn60@gmail.com>
2020-10-21 16:11:38 -0600

    now you can merge this bro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 // import react
 import React from 'react';
 // import Masonry layout component
-import Grid from '../../components/Grid';
+// import Grid from '../../components/Grid';
 
 // create and export Home page
 const Home = () => (
