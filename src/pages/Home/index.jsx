ebe17660ae42a76ec900d75445d00ffbe817417b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-21 15:47:59 -0500

    fix last commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,7 +12,7 @@ const Home = () => (
     <Header />
 		I&apos;m the Home page :/
     <Hero
-      caption="Top 10 amazing things 2020"
+      caption="Top 10 amazing things 2020 <3"
       background="https://images.unsplash.com/photo-1601907532353-3c9176919f38?ixlib=rb-1.2.1&auto=format&fit=crop&w=982&q=80"
     />
   </>
