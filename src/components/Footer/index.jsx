4bd78b66b30d76d13ac65960401a375f0ce20dcf
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-21 19:26:23 -0500

    Add link component from react router dom to footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,32 +1,34 @@
 // import react
 import React from 'react';
+// import link component
+import { Link } from 'react-router-dom';
 // import icons
 const Facebook = 'https://www.flaticon.es/svg/static/icons/svg/733/733605.svg';
 const Twitter = 'https://www.flaticon.es/svg/static/icons/svg/733/733635.svg';
 const Instagram = 'https://www.flaticon.es/svg/static/icons/svg/87/87390.svg';
 // import logo
-import Logo from '../../assets/Logo'
+import Logo from '../../assets/Logo';
 // import styles
 import './styles.styl';
 
 const Footer = () => (
   <footer className="footer">
     <div className="footer__social">
-      <img className="footer__social--icons" src={Facebook} alt="Facebook"/>
-      <img className="footer__social--icons" src={Twitter} alt="Twitter"/>
-      <img className="footer__social--icons" src={Instagram} alt="Instagram"/>
+      <img className="footer__social--icons" src={Facebook} alt="Facebook" />
+      <img className="footer__social--icons" src={Twitter} alt="Twitter" />
+      <img className="footer__social--icons" src={Instagram} alt="Instagram" />
     </div>
-    <Logo className="footer__logo"/>
+    <Logo className="footer__logo" />
     <div className="footer__subtitle">
-      <h2>
-        About
-      </h2>
-      <h2>
-        Team
-      </h2>
+      <Link to="/about">
+        <h2>About</h2>
+      </Link>
+      <Link to="/team">
+        <h2>Team</h2>
+      </Link>
     </div>
     <p className="footer__comments">
-      2020 by Masters INC. Proudly created with React JS
+			2020 by Masters INC. Proudly created with React JS
     </p>
   </footer>
 );
