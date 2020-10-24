53aefea4244ba4bec8fcaca364fec2add10929c8
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-23 21:53:38 -0500

    Update test to grid with context provider

--- src/__tests__/components/Grid.test.js
+++ src/__tests__/components/Grid.test.js
@@ -1,30 +1,41 @@
 // import react
 import React from 'react';
 // import shallow
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 //import card component
 import Grid from '../../components/Grid';
+// import context
+// import context provider
+import { ContextProvider } from '../../utils/Context/index.jsx';
 
 describe('<Grid/>', () => {
-  const wrapper = shallow(<Grid/>);
+  const wrapper = mount(
+    <ContextProvider>
+      <Grid />
+    </ContextProvider>,
+  );
 
   it('Render the Grid component', () => {
     expect(wrapper.exists()).toBe(true);
-  })
+  });
 
   it('Is there a class masonry component?', () => {
     expect(wrapper.find('.masonry').exists()).toBe(true);
-  })
+  });
 
   it('Only one child?', () => {
     expect(wrapper.find('.masonry').children().length).toBe(1);
-  })
+  });
 
   it('Is there a ResponsiveMasonry element?', () => {
     expect(wrapper.find('ResponsiveMasonry').exists());
-  })
+  });
 
   it('Is there a masonry-layout element?', () => {
     expect(wrapper.find('Masonry').exists()).toBe(true);
-  })
-})
+  });
+
+  it('Is there a context provider?', () => {
+    expect(wrapper.find('ContextProvider').exists()).toBe(true);
+  });
+});
--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,15 +1,17 @@
 // import React
-import React from 'react';
+import React, { useContext } from 'react';
 // import masonry
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 // import card
 import Card from '../Card/index.jsx';
+// import context
+import { Context } from '../../utils/Context/index.jsx'
 // import styles
 import './styles.styl';
 
 const Grid = () => {
 
-  const cards = []
+  const { news } = useContext(Context)
 
   return (
     <div className="masonry">
@@ -17,7 +19,7 @@ const Grid = () => {
         columnsCountBreakPoints={{ 480: 1, 700: 2, 1000: 2 }}
       >
         <Masonry gutter="20px">
-          {cards.map((card, i) => <Card key={i} title={card?.title} image={card?.image} />)}
+          {news.map((card, i) => <Card key={i} title={card?.title} image={card?.image} />)}
         </Masonry>
       </ResponsiveMasonry>
     </div>
