907e56797c31f352777da536f0c362a2de79cbd1
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-24 12:34:51 -0500

    Update footer tests to test the links

--- src/__tests__/components/Footer.test.js
+++ src/__tests__/components/Footer.test.js
@@ -1,7 +1,9 @@
 // import react
 import React from 'react';
 // import shallow
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+// import router
+import { HashRouter as Router } from 'react-router-dom';
 // import toJson
 import toJson from 'enzyme-to-json';
 // import app component
@@ -16,5 +18,35 @@ describe('<Footer />', () => {
 
   it('matches the snap shot', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
+
+describe('<Footer /> mount', () => {
+  const wrapper = mount(
+    <Router>
+      <Footer />
+    </Router>,
+  );
+
+  it('is render the Footer?', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('are there a Footer social  tag?', () => {
+    expect(wrapper.find('.footer__social').children().length).toBe(3);
+  });
+
+  it('should Footer have the correct text?', () => {
+    expect(wrapper.find('.footer__comments').text()).toBe(
+      '2020 by Masters INC. Proudly created with React JS',
+    );
+  });
+
+  it('should Footer have two about link component?', () => {
+    expect(wrapper.find('Link').first().prop('to')).toBe('/about');
+  });
+
+  it('should Footer link component has the correct route?', () => {
+    expect(wrapper.find('Link').last().prop('to')).toBe('/team');
   });
 });
