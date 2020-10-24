18ebda5fd57a5fe3785119b262d5198f3a389f12
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-24 18:36:03 -0500

    Update tests

--- src/__tests__/components/Article.test.js
+++ src/__tests__/components/Article.test.js
@@ -1,45 +1,40 @@
 // import react
 import React from 'react';
 // import shallow
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 // import toJson
 import toJson from 'enzyme-to-json';
+// import router
+import { HashRouter as Router } from 'react-router-dom';
 // import app component
 import Article from '../../pages/Article';
+// import context provider
+import { ContextProvider } from '../../utils/Context/index.jsx';
 
 describe('<Article ...props/>', () => {
-  const wrapper = shallow(
-    <Article
-      title="title"
-      subtitle="subtitle"
-      author="Juan"
-      date="00/00/00"
-      image="random"
-      text="lorem"
-    />,
+  const wrapper = mount(
+    <ContextProvider>
+      <Router>
+        <Article />
+      </Router>
+    </ContextProvider>,
   );
 
   it('is render the Article?', () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  it('is render the title, subtitle, author and date properties?', () => {
-    expect(wrapper.find('.content__headline').text()).toEqual('titlesubtitleJuan - 00/00/00')
+  it('is render the headline?', () => {
+    expect(wrapper.find('.content__headline').exists()).toBe(true)
   });
 
+  wrapper.setProps({ $news: { title: 'random', image: 'random' } })
+
   it('is render the src?', () => {
-    expect(wrapper.find('.content__image').first().props().src).toBe('random');
+    expect(wrapper.find('.content__image').prop('src')).toBe();
   });
 
   it('matches the snap shot', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
-  });
-});
-
-describe('<Hero />', () => {
-  const wrapper = shallow(<Article />);
-
-  it('are default type works?', () => {
-    expect(wrapper.exists()).toBeTruthy();
   });
 });
--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,34 +1,47 @@
 // import react
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+// use params hooks
+import { useParams } from 'react-router-dom'
+// import context
+import { Context } from '../../utils/Context/index.jsx';
 // import styles
 import './styles.styl';
 
 // create and export Home page
-const Article = ({ title, subtitle, author, date, image, text }) => (
-  <article className="container">
+const Article = () => {
+  // use state
+  const [new$, setNew$] = useState()
+  // use context
+  const { news } = useContext(Context)
+  // use ref hooks
+  const { name } = useParams()
+  // use effect
+  useEffect(() => {
+    setNew$(news?.find(item => item?.title === name && item))
+  }, [])
+
+  return (<article className="container">
     <section className="content">
       <div className="content__headline">
-        <h1>{title}</h1>
-        <h2>{subtitle}</h2>
-        <span>{author} - {date}</span>
+        <h1>{new$?.title}</h1>
       </div>
-      <img src={image} className="content__image"/>
+      <img src={new$?.image} className="content__image"/>
       <div className="content__text">
-        <p>
-          {text}
-        </p>
-        <br/>
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in dui ultricies, tristique justo eu, ornare nisl. Phasellus accumsan vel justo eu volutpat. Mauris augue ipsum, accumsan non nisl ut, rutrum iaculis enim. Pellentesque risus est, mollis sed porttitor sed, efficitur quis enim. Etiam pulvinar lobortis lectus, nec dapibus ex cursus nec. Integer volutpat rutrum turpis ac viverra.
         </p>
         <br/>
         <p>
-        Sed eu tincidunt risus. Cras at lectus viverra, dictum erat nec, maximus ipsum. Aenean ante arcu, aliquam ut lacinia malesuada, pulvinar ac mi. Curabitur iaculis eu dolor nec consectetur. Aliquam semper volutpat diam, id faucibus leo aliquam id. Phasellus mattis in leo in bibendum. Ut aliquet velit purus, eu viverra eros aliquam sed. Vestibulum maximus turpis vel eros vestibulum, in pharetra dui vestibulum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in dui ultricies, tristique justo eu, ornare nisl. Phasellus accumsan vel justo eu volutpat. Mauris augue ipsum, accumsan non nisl ut, rutrum iaculis enim. Pellentesque risus est, mollis sed porttitor sed, efficitur quis enim. Etiam pulvinar lobortis lectus, nec dapibus ex cursus nec. Integer volutpat rutrum turpis ac viverra.
+        </p>
+        <br/>
+        <p>
+      Sed eu tincidunt risus. Cras at lectus viverra, dictum erat nec, maximus ipsum. Aenean ante arcu, aliquam ut lacinia malesuada, pulvinar ac mi. Curabitur iaculis eu dolor nec consectetur. Aliquam semper volutpat diam, id faucibus leo aliquam id. Phasellus mattis in leo in bibendum. Ut aliquet velit purus, eu viverra eros aliquam sed. Vestibulum maximus turpis vel eros vestibulum, in pharetra dui vestibulum.
         </p>
 
       </div>
     </section>
-  </article>
-);
+  </article>)
+};
 
 export default Article;
