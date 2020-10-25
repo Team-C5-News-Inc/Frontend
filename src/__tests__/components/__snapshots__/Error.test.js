00c8e6e3c14fabb343dbab19133740d51f59ef76
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-24 20:56:01 -0500

    Create error tests

---
+++ src/__tests__/components/__snapshots__/Error.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import shallow
+import { shallow } from 'enzyme';
+// import app component
+import Error from '../../../components/Error/index.jsx';
+
+describe('<Error />', () => {
+  const wrapper = shallow(<Error />);
+
+  it('is render the Error?', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('is a container class?', () => {
+    expect(wrapper.find('.container').exists()).toBe(true);
+  });
+
+  it('are there correct the title?', () => {
+    expect(wrapper.find('h1').text()).toBe('');
+  });
+
+  it('are have two child?', () => {
+    expect(wrapper.children().length).toBe(2);
+  });
+});
