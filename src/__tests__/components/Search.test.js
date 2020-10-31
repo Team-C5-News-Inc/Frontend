870eb84fe7fa9bb5fcb77d3b600606b7a16db60f
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-30 18:14:42 -0600

    Improve tests ans use search

--- src/__tests__/components/Search.test.js
+++ src/__tests__/components/Search.test.js
@@ -4,11 +4,17 @@ import React from 'react';
 import { mount } from 'enzyme';
 // import toJson
 import toJson from 'enzyme-to-json';
+// import context provider
+import { ContextProvider } from '../../utils/Context/index.jsx';
 // import app component
 import Search from '../../components/Search/index.jsx';
 
 describe('<Search />', () => {
-  const wrapper = mount(<Search />);
+  const wrapper = mount(
+    <ContextProvider>
+      <Search />
+    </ContextProvider>,
+  );
 
   it('is the Search rendering?', () => {
     expect(wrapper.exists()).toBe(true);
@@ -26,6 +32,20 @@ describe('<Search />', () => {
     wrapper.find('.search').simulate('click')
 
     expect(wrapper.find('.searchformOff').props().className).toBe('searchformOff slide_search-left searchform');
+  });
+
+  it('is there a class named slide left?', () => {
+    wrapper.find('.search').simulate('click')
+
+    expect(wrapper.find('.searchformOff').props().className).toBe('searchformOff off');
+  });
+
+  it('is there search value?', () => {
+    const event = { target: { name: 'search', value: 'random' } };
+
+    wrapper.find('input').simulate('change', event);
+
+    expect(wrapper.find('input').prop('value')).toBe('random');
   });
 
   it('matches the snap shot', () => {
